67a0c8fced3b9f4e9ce327475c700d180ea1361c
Tim Graves <tim.graves@timgraves.info>
2020-01-05 16:01:34 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,50 @@
+const express          = require('express'),
+      expressSanitizer = require('express-sanitizer'),
+      app              = express(),
+      mongoose         = require('mongoose'),
+      flash            = require('connect-flash'),
+      passport         = require('passport'),
+      LocalStrategy    = require('passport-local'),
+      methodOverride   = require('method-override'),
+      User             = require('./models/user'),
+      Announcement     = require('./models/announcement');
+
+// REQUIRING ROUTES
+const indexRoutes        = require('./routes/index'),
+      adminRoutes        = require('./routes/admin'),
+      userRoutes         = require('./routes/user'),
+      announcementRoutes = require('./routes/announcement');
+
+mongoose.connect(process.env.DBURL);
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.use(expressSanitizer());
+app.use(methodOverride('_method'));
+app.use(flash());
+
+// PASSPORT CONFIGURATION
+app.use(require('express-session')({
+   secret: 'Greetings netizens. Your overlord has arrived.',
+   resave: false,
+   saveUninitialized: false,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use( (req, res, next) => {
+   res.locals.currentUser = req.user;
+   res.locals.error = req.flash('error');
+   res.locals.success = req.flash('success');
+   next();
+});
+
+app.use(indexRoutes);
+app.use('/admin', adminRoutes);
+app.use('/user', userRoutes);
+app.use('/announcements', announcementRoutes);
+
+app.listen(process.env.PORT);
---
+++ middleware/index.js
@@ -0,0 +1,44 @@
+const Announcement = require('../models/announcement');
+const User = require('../models/user');
+
+const middlewareObj = {};
+
+middlewareObj.isLoggedIn = (req, res, next) => {
+   if (req.isAuthenticated()) {
+      return next();
+   }
+
+   req.flash('error', "You need to be logged in to do that!");
+   res.redirect('/login');
+};
+
+middlewareObj.isAdmin = (req, res, next) => {
+   if (req.isAuthenticated()) {
+      if (req.user.isAdmin) {
+         return next();
+      } else {
+         req.flash('error', 'You don\'t have permission to do that. Ask an admin for help.');
+         res.redirect('back');
+      }
+   }
+
+   req.flash('error', 'You need to be logged in to do that!');
+   res.redirect('/login');
+};
+
+middlewareObj.isEditor = (req,res,next) {
+   if (req.isAuthenticated()) {
+      if (req.user.isEditor || req.user.isAdmin) {
+         return next();
+      } else {
+         req.flash('error', 'You don\'t have permission to do that.');
+         res.redirect('back');
+      }
+   }
+   
+   req.flash('error', 'You need to be logged in to do that!');
+   res.redirect('/login');
+};
+
+module.exports = middlewareObj;
+
