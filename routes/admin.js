e13251981effe4495a803cd5bbf7a7c166beafd3
Tim Graves <tim.graves@timgraves.info>
2020-02-20 21:38:35 -0500

    Corrected typo in group model require in routes admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,7 @@ const express      = require('express'),
       passport     = require('passport'),
       User         = require('../models/user'),
       Announcement = require('../models/announcement'),
-      Group        = require('..models/group'),
+      Group        = require('../models/group'),
       middleware   = require('../middleware');
 
 // ADMIN DASHBOARD LANDING
